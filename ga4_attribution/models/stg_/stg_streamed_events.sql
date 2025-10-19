6c92fca4b016d799e20da243c70a598db8de9501
Ajaikumar A <ajaikumar2038@gmail.com>
2025-10-19 18:27:50 +0530

    Added incremental model to merge streamed events into raw.

---
+++ ga4_attribution/models/stg_/stg_streamed_events.sql
@@ -0,0 +1,31 @@
+{{config(
+    materialized = "incremental", 
+    unique_key = "event_id", 
+    partition_key = {"field": "event_date", "data_type": "date"}
+)}}
+
+-- Read buffer table
+with buffer as (
+    select 
+        event_id, 
+        event_timestamp, 
+        user_pseudo_id, 
+        event_name, 
+        source, 
+        medium, 
+        campaign, 
+        timestamp_micros(cast(event_timestamp as int64)) as event_ts, 
+        date(timestamp_micros(cast(event_timestamp as int64))) as event_date
+    from 
+        '{{var('project_id')}}.ga4_models.stream_events_buffer'
+)
+select 
+    *
+from 
+    buffer
+
+{% if is_incremental() %}
+    where event_id is not null and event_ts > (
+        select coalesce(max(event_ts), timestamp('1970-01-01')) from {{this}}
+    )
+{% endif %}
